579d0b4f476840c13a809394234bf0c13a6bbcdb
Lt-Henry <quiqueiii@gmail.com>
2018-06-13 21:52:06 +0200

    wip wavefront loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,114 @@
 
 #include <blaster/raster.h>
+#include <blaster/vector.h>
 
 #include <SDL2/SDL.h>
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 #define WIDTH 800
 #define HEIGHT 600
+
+struct Triangle
+{
+    int v[3];
+    int n[3];
+};
+
+struct Mesh
+{
+    vector<bl_vector_t> vertices;
+    vector<bl_vector_t> normals;
+    vector<Triangle> triangles;
+};
+
+vector<string> split(string line,char sep=' ')
+{
+    vector<string> tokens;
+    
+    bool knee=false;
+    string tmp;
+
+    for (char c : line) {
+
+        if (c==sep) {
+            if (knee==true) {
+                tokens.push_back(tmp);
+                knee=false;
+                tmp="";
+            }
+        }
+        else {
+            tmp=tmp+c;
+            knee=true;
+        }
+    }
+
+    if (tmp!="") {
+        tokens.push_back(tmp);
+    }
+    
+    return tokens;
+}
+
+Mesh* load_obj(char* filename)
+{
+    clog<<"loading: "<<filename<<endl;
+    
+    fstream fs;
+    
+    setlocale(LC_NUMERIC,"C");
+    
+    fs.open(filename,fstream::in);
+    
+    Mesh* mesh = new Mesh();
+    
+    while (!fs.eof()) {
+        string line;
+        getline(fs,line);
+        
+        vector<string> tmp = split(line);
+        
+        if (tmp.size()<=0) {
+            continue;
+        }
+        
+        if (tmp[0]=="v") {
+            bl_vector_t v;
+            
+            v.x=stof(tmp[1]);
+            v.y=stof(tmp[2]);
+            v.z=stof(tmp[3]);
+            v.w=1;
+            
+            mesh->vertices.push_back(v);
+        }
+        
+        if (tmp[0]=="f") {
+            Triangle tri;
+            
+            vector<string> tmp2 = split(tmp[1],'/');
+            tri.v[0]=stoi(tmp2[0]);
+            
+            tmp2 = split(tmp[2],'/');
+            tri.v[1]=stoi(tmp2[0]);
+            
+            tmp2 = split(tmp[3],'/');
+            tri.v[2]=stoi(tmp2[0]);
+            
+            mesh->triangles.push_back(tri);
+        }
+    }
+    
+    fs.close();
+    
+    return mesh;
+}
+
 
 int main(int argc,char* argv[])
 {
@@ -17,6 +117,11 @@ int main(int argc,char* argv[])
     SDL_Texture* texture;
     
     clog<<"Blaster-demo"<<endl;
+    
+    Mesh* mesh = load_obj(argv[1]);
+    
+    clog<<"vertices: "<<mesh->vertices.size()<<endl;
+    clog<<"triangles: "<<mesh->triangles.size()<<endl;
     
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow("blaster", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
