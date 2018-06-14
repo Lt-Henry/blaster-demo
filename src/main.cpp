849745eeed0ef1537bb39615514948d918618d95
Lt-Henry <quiqueiii@gmail.com>
2018-06-14 16:59:45 +0200

    drawing mesh as a point cloud

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 
 #include <blaster/raster.h>
 #include <blaster/vector.h>
+#include <blaster/vbo.h>
 
 #include <SDL2/SDL.h>
 
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
 
@@ -109,12 +111,37 @@ Mesh* load_obj(char* filename)
     return mesh;
 }
 
+bl_vbo_t* build_vbo(Mesh* mesh)
+{
+    bl_vbo_t* vbo;
+    
+    struct point_t {
+        bl_vector_t p;
+        bl_color_t c;
+    };
+    
+    vbo=bl_vbo_new(mesh->vertices.size(),sizeof(point_t));
+    
+    for (int n=0;n<mesh->vertices.size();n++) {
+        struct point_t point = {0};
+        
+        point.p=mesh->vertices[n];
+        
+        bl_vbo_set(vbo,n,&point);
+    }
+    
+    return vbo;
+}
+
 
 int main(int argc,char* argv[])
 {
     SDL_Window* window;
     SDL_Renderer* renderer;
     SDL_Texture* texture;
+    
+    bl_raster_t* raster;
+    bl_vbo_t* dino;
     
     clog<<"Blaster-demo"<<endl;
     
@@ -123,10 +150,33 @@ int main(int argc,char* argv[])
     clog<<"vertices: "<<mesh->vertices.size()<<endl;
     clog<<"triangles: "<<mesh->triangles.size()<<endl;
     
+    raster=bl_raster_new(WIDTH,HEIGHT);
+    
+    bl_color_t clear_color;
+    bl_color_set(&clear_color,0.9,0.9,0.9,1.0);
+    
+    bl_raster_set_clear_color(raster,&clear_color);
+    bl_raster_clear(raster);
+    
+    dino=build_vbo(mesh);
+    
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow("blaster", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WIDTH,HEIGHT);
+    
+    
+    auto tfps = std::chrono::steady_clock::now();
+    
+    double dfps=0;
+    int fps=0;
+    double time_clear=0;
+    double time_raster=0;
+    double time_upload=0;
+    double time_present=0;
+    
+    float angle=0;
+    float aspeed=0.1f;
     
     bool quit_request=false;
     
@@ -145,9 +195,71 @@ int main(int argc,char* argv[])
             } // switch
         } // while
         
+        
+        //render here
+        auto t1 = std::chrono::steady_clock::now();
+        
+        bl_raster_clear(raster);
+        
+        auto t2 = std::chrono::steady_clock::now();
+        time_clear+=std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
+        
+        
+        bl_matrix_stack_load_identity(raster->projection);
+        bl_matrix_stack_frustum(raster->projection,
+        -1,1,-1,1,1,100);
+        
+        bl_matrix_stack_load_identity(raster->modelview);
+        bl_matrix_stack_translate(raster->modelview,0.0f,-2.0f,-20);
+
+        angle+=0.01f;
+        bl_matrix_stack_rotate_y(raster->modelview,angle);
+        
+        bl_raster_draw_points(raster,dino);
+        bl_raster_update(raster);
+        
+        SDL_Rect rect;
+
+        rect.x=0;
+        rect.y=0;
+        rect.w=WIDTH;
+        rect.h=HEIGHT;
+
+        auto t3 = std::chrono::steady_clock::now();
+        
+        SDL_UpdateTexture(texture,&rect,(void*)raster->color_buffer->data,WIDTH*sizeof(uint32_t));
+        
+        auto t4 = std::chrono::steady_clock::now();
+        time_upload+=std::chrono::duration_cast<std::chrono::milliseconds>(t4-t3).count();
+        
         SDL_RenderCopy(renderer, texture, NULL, NULL);
         SDL_RenderPresent(renderer);
+        
+        auto t5 = std::chrono::steady_clock::now();
+        time_present+=std::chrono::duration_cast<std::chrono::milliseconds>(t5-t4).count();
+        
+        fps++;
+        dfps=std::chrono::duration_cast<std::chrono::milliseconds>(t5-tfps).count();
+        
+        if (dfps>1000) {
+            clog<<"fps: "<<fps<<endl;
+            clog<<"clear: "<<time_clear<<" ms"<<endl;
+            clog<<"upload: "<<time_upload<<" ms"<<endl;
+            clog<<"present: "<<time_present<<" ms"<<endl;
+
+            aspeed=((2.0f*3.14f)*fps)/1000.0f;
+
+            dfps=0;
+            fps=0;
+            time_clear=0;
+            time_upload=0;
+            time_present=0;
+            
+            tfps = std::chrono::steady_clock::now();
+        }
     }
+    
+    bl_raster_delete(raster);
     
     return 0;
 }
